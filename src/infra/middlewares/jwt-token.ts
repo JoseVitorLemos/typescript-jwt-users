681e22574f7c85240d788a1323619c73c81e5a8c
José Vitor <jose81073175@gmail.com>
2022-02-04 04:23:59 -0300

    feat: create middleware verify acess token

---
+++ src/infra/middlewares/jwt-token.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import { Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+export function verifyJWT(req: any, res: Response, next: NextFunction) {
+	const token: string = req.headers['x-acess-token'] as string
+
+	jwt.verify(token, process.env.TOKEN as string, (err, decoded: any) => {
+		if(err) return res.status(401).json({ statusCode: 401, message: 'Autentication fail' }).end()
+
+		req.userId = decoded.userId
+		next()
+	})
+}
+
