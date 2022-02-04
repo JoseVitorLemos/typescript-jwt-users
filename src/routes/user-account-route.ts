230480b11460bdfc827721c9632c1b1b87e93d5e
José Vitor <jose81073175@gmail.com>
2022-02-04 04:19:08 -0300

    fix: Remove .env file

--- src/routes/user-account-route.ts
+++ src/routes/user-account-route.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import knex from '../infra/database/connection'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+import { verifyJWT } from '../infra/middlewares/jwt-token'
 
 const userAccount = Router()
 
-userAccount.get('/:id', async (req, res) => {
+userAccount.get('/:id', verifyJWT,async (req, res) => {
 	const { id } = req.params
 
 	const user = await knex('user_account')
@@ -12,12 +15,11 @@ userAccount.get('/:id', async (req, res) => {
 
 	if(user) {
 		return res.status(200).json(user)
-	} else {
-		return res.status(404).json({
-			statusCode: 404,
-			message: 'Users not found'
-		})
 	}
+	return res.status(404).json({
+		statusCode: 404,
+		message: 'Users not found'
+	})
 })
 
 userAccount.post('/signup', async (req, res) => {
@@ -48,17 +50,21 @@ userAccount.post('/login', async (req, res) => {
 
 	const user = await knex('user_account')
 		.where('user_account.email', email)
-		.select('user_account.email', 'user_account.password').first()
+		.select('user_account.id', 'user_account.email', 'user_account.password').first()
 
 	if(!user) return res.status(400).json({ statusCode: 400, message: 'Email dont registered' })
 
 	if(user.email == email && user.password == password) {
-		return res.status(200).json('Login success !')
-	} else {
-		return res.status(400).json({ 
-			statusCode: 400, 
-			message: 'Autentication fail' })
+
+		const token = jwt.sign({ userId: user.id }, process.env.TOKEN as string, { expiresIn: 600 })
+
+		return res.status(200).json({ auth: true, token })
 	}
+
+	return res.status(401).json({ 
+		statusCode: 401, 
+		message: 'Autentication fail' 
+	}).end()
 })
 
 export default userAccount
