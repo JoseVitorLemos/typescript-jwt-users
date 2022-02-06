d71bfc1e68ae668577a85f0e456f5ccbb4a0320c
José Vitor <jose81073175@gmail.com>
2022-02-05 22:45:24 -0300

    refactor: validate token jwt by more options

--- src/infra/middlewares/jwt-token.ts
+++ src/infra/middlewares/jwt-token.ts
@@ -3,7 +3,7 @@ import { Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
 export function verifyJWT(req: any, res: Response, next: NextFunction) {
-	const token: string = req.headers['x-acess-token'] as string
+	const token: string = req.headers['x-acess-token'] || req.query.token || req.body.token
 
 	jwt.verify(token, process.env.TOKEN as string, (err, decoded: any) => {
 		if(err) return res.status(401).json({ statusCode: 401, message: 'Autentication fail' }).end()
