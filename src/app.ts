658a9f4da1613696ae6dcb360fe1322c613922a5
José Vitor <jose81073175@gmail.com>
2022-02-09 21:58:14 -0300

    refactor: change routes to a class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express'
+import UserAccountRoute from './routes/user-account-route'
 
 class App {
 	private app: Application
 
 	constructor() {
-		this.app = express()	
+		this.app = express()
 		this.bodyParse()
+		this.setupRoutes()
 	}
 
 	start(port: string | number = 3000) {
@@ -13,7 +15,11 @@ class App {
 	}
 
 	bodyParse() {
-	 return this.app.use(express.json())
+	 	return this.app.use(express.json())
+	}
+
+	setupRoutes() {
+		this.app.use('/account', UserAccountRoute.getUserRoute())	
 	}
 }
 
--- src/routes/user-account-route.ts
+++ src/routes/user-account-route.ts
@@ -5,106 +5,119 @@ import { verifyJWT } from '../infra/middlewares/jwt-token'
 import bcrypt from 'bcrypt'
 import generateToken from '../helper/generate-token'
 
-const userAccount = Router()
+class UserAccount {
+	private router: Router
 
-userAccount.get('/:id', verifyJWT, async (req, res) => {
-	const { id } = req.params
-
-	const user = await knex('user_account')
-		.where('user_account.id', id)
-		.select('user_account.id', 'user_account.email', 'user_account.created_at', 'user_account.updated_at').first()
-
-	if(user) {
-		return res.status(200).json(user)
-	}
-	return res.status(404).json({
-		statusCode: 404,
-		message: 'Users not found'
-	})
-})
-
-userAccount.post('/signup', async (req, res) => {
-	const { email, password }	= req.body
-
-	const hashedPassword = await bcrypt.hash(password, 10)
-
-	const newUser = {
-		email,
-		password: hashedPassword,
-		created_at: new Date()
+	constructor() {
+		this.router = Router()
+		this.userRoutes()
 	}
 
-	const findEmail = await knex('user_account').where('user_account.email', email).first()
-
-	if(findEmail) return res.status(400).json({ statusCode: 400, message: 'Email already registered' })
-
-	const id = await knex('user_account').insert(newUser).returning('id').then(prop => prop[0].id)
-
-	const user = await knex('user_account')
-		.where('user_account.id', id)
-		.select('user_account.id', 'user_account.email', 'user_account.created_at', 'user_account.updated_at').first()
-
-	return res.status(201).json(user)
-})
-
-userAccount.post('/login', async (req, res) => {
-	const { email, password } = req.body
-
-	const user = await knex('user_account')
-		.where('user_account.email', email)
-		.select('user_account.id', 'user_account.email', 'user_account.password').first()
-
-	if(!user) return res.status(400).json({ statusCode: 400, message: 'Email dont registered' })
-
-	const checkPassword = await bcrypt.compare(password, user.password)
-
-	if(checkPassword) { 
-		const token = generateToken(user.id)
-
-		return res.status(200).json(token)
+	getUserRoute() {
+		return this.router
 	}
- 
-	return res.status(401).json({ 
-		statusCode: 401, 
-		message: 'Invalid password' 
-	}).end()
-})
 
-userAccount.put('/user-account/:id', verifyJWT, async (req, res) => {
-	const { oldPassword, newPassword } = req.body
+	private userRoutes () {
+		this.router.get('/:id', verifyJWT, async (req, res) => {
+			const { id } = req.params
 
-	const { password } = await knex('user_account')
-		.where('user_account.id', req.params.id)
-		.select('user_account.password').first()
+			const user = await knex('user_account')
+			.where('user_account.id', id)
+			.select('user_account.id', 'user_account.email', 'user_account.created_at', 'user_account.updated_at').first()
 
-	const equalPassword = await bcrypt.compare(newPassword, password)
-
-	if(equalPassword) return res.status(400).json({
-		statusCode: 400,
-		message: `Password it's equal`
-	})
-	
-	const checkPassword = await bcrypt.compare(oldPassword, password)
-
-	const hashedPassword = await bcrypt.hash(newPassword, 10)
-
-	if(checkPassword) {
-		try {
- 			await knex('user_account').where('user_account.id', req.params.id).update({ password: hashedPassword })
-
-			return res.status(200).json({
-				statusCode: 200,
-				message: 'Updated success password'
+			if(user) {
+				return res.status(200).json(user)
+			}
+			return res.status(404).json({
+				statusCode: 404,
+				message: 'Users not found'
 			})
- 		} catch (err) {
-			return res.send(err).end()
- 		}
+		})
+
+		this.router.post('/signup', async (req, res) => {
+			const { email, password }	= req.body
+
+			const hashedPassword = await bcrypt.hash(password, 10)
+
+			const newUser = {
+				email,
+				password: hashedPassword,
+				created_at: new Date()
+			}
+
+			const findEmail = await knex('user_account').where('user_account.email', email).first()
+
+			if(findEmail) return res.status(400).json({ statusCode: 400, message: 'Email already registered' })
+
+			const id = await knex('user_account').insert(newUser).returning('id').then(prop => prop[0].id)
+
+			const user = await knex('user_account')
+			.where('user_account.id', id)
+			.select('user_account.id', 'user_account.email', 'user_account.created_at', 'user_account.updated_at').first()
+
+			return res.status(201).json(user)
+		})
+
+		this.router.post('/login', async (req, res) => {
+			const { email, password } = req.body
+
+			const user = await knex('user_account')
+			.where('user_account.email', email)
+			.select('user_account.id', 'user_account.email', 'user_account.password').first()
+
+			if(!user) return res.status(400).json({ statusCode: 400, message: 'Email dont registered' })
+
+			const checkPassword = await bcrypt.compare(password, user.password)
+
+			if(checkPassword) { 
+				const token = generateToken(user.id)
+
+				return res.status(200).json(token)
+			}
+
+			return res.status(401).json({ 
+				statusCode: 401, 
+				message: 'Invalid password' 
+			}).end()
+		})
+
+		this.router.put('/user-account/:id', verifyJWT, async (req, res) => {
+			const { oldPassword, newPassword } = req.body
+
+			const { password } = await knex('user_account')
+			.where('user_account.id', req.params.id)
+			.select('user_account.password').first()
+
+			const equalPassword = await bcrypt.compare(newPassword, password)
+
+			if(equalPassword) return res.status(400).json({
+				statusCode: 400,
+				message: `Password it's equal`
+			})
+
+			const checkPassword = await bcrypt.compare(oldPassword, password)
+
+			const hashedPassword = await bcrypt.hash(newPassword, 10)
+
+			if(checkPassword) {
+				try {
+ 					await knex('user_account').where('user_account.id', req.params.id).update({ password: hashedPassword })
+
+					return res.status(200).json({
+						statusCode: 200,
+						message: 'Updated success password'
+					})
+ 				} catch (err) {
+					return res.send(err).end()
+ 				}
+			}
+
+			return res.status(204).json({
+				statusCode: 204,
+				message: 'Invalid password'
+			})
+		})
 	}
+}
 
-	return res.status(204).json({
-		statusCode: 204,
-		message: 'Invalid password'
-	})
-})
-
-export default userAccount
+export default new UserAccount
