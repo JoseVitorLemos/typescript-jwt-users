2cdb5fbaf20dd9feed1902b1991499e3e85a34cf
José Vitor <jose81073175@gmail.com>
2022-02-09 21:38:05 -0300

    refactor: add body parse json in app

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,15 @@ class App {
 
 	constructor() {
 		this.app = express()	
+		this.bodyParse()
 	}
 
 	start(port: string | number = 3000) {
 		return this.app.listen(port, () => console.log(`Server running at ${port}`))
+	}
+
+	bodyParse() {
+	 return this.app.use(express.json())
 	}
 }
 
--- src/infra/middlewares/body-parse-json.ts
+++
@@ -1,6 +0,0 @@
-import { json } from 'express'
-
-const bodyParse = json()
-
-export default bodyParse
-
