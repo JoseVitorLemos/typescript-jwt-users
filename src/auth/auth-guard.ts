d79fec6ae19fad765811c26f7f93295e39afb94d
José Vitor <jose81073175@gmail.com>
2022-02-13 22:02:55 -0300

    feat: add bearer option auth

--- src/auth/auth-guard.ts
+++ src/auth/auth-guard.ts
@@ -7,7 +7,7 @@ interface userRequest extends Request {
 }
 
 export function verifyJWT(req: userRequest, res: Response, next: NextFunction) {
-	const token: string = req.headers['x-access-token'] || req.query.token || req.body.token
+	const token: string = (req.headers['x-access-token'] || req.query.token || req.body.token || req.headers.authorization)?.split(' ')[1]
 
 	jwt.verify(token, process.env.TOKEN as string, (err, decoded: any) => {
 		if(err) return res.status(401).json({ statusCode: 401, auth: false, message: 'Unauthorized' })
