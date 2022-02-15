20452344609ba86ec4de83f771ab19f7334172fe
José Vitor <jose81073175@gmail.com>
2022-02-15 02:35:02 -0300

    feat: only use bearer authorization

--- src/auth/auth-guard.ts
+++ src/auth/auth-guard.ts
@@ -7,7 +7,7 @@ interface userRequest extends Request {
 }
 
 export function AuthGuard(req: userRequest, res: Response, next: NextFunction) {
-	const token: string = (req.headers['x-access-token'] || req.query.token || req.body.token || req.headers.authorization)?.split(' ')[1]
+	const token: string = req.headers.authorization?.split(' ')[1] as string
 
 	jwt.verify(token, process.env.TOKEN as string, (err, decoded: any) => {
 		if(err) return res.status(401).json({ statusCode: 401, auth: false, message: 'Unauthorized' })
